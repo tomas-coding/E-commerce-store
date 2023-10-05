2f804ed1f84e01f19f9c666789d202f2a400e327
Tomas Lovato <lovato.tomas94@gmail.com>
2023-10-05 10:35:44 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "utfs.io"
+        ]
+    }
+}
 
 module.exports = nextConfig
